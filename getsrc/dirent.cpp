b57230a7ea19fe6a59b687f05b5176c89622a2a0
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2024-01-18 23:41:27 +0000

    Updated getsrc.cpp to use html links.

--- getsrc/dirent.cpp
+++ getsrc/dirent.cpp
@@ -1,6 +1,7 @@
 #include<algorithm>
 #include<array>
 #include<filesystem>
+#include<format>
 #include<iostream>
 #include<vector>
 std::array<std::string, 6>allowed_exts{"s", "asm", "h", "c", "hpp", "cpp"};
@@ -8,10 +9,14 @@ bool validate_ext(const std::string &ext)
 {
 	return std::find(allowed_exts.cbegin(), allowed_exts.cend(), ext.substr(1)) != allowed_exts.cend();
 }
+std::string mkelem(const std::string &file)
+{
+    return std::format("<div><a href=\"{}\">{}</a></div>", file, file);
+}
 void dirent()
 {
     using namespace std::filesystem;
-    std::cout << "HTTP/1.1 200 OK\r\n" << "connection: close\r\n" << "content-type: text/plain\r\n\r\n";
+    std::cout << "HTTP/1.1 200 OK\r\n" << "connection: close\r\n" << "content-type: text/html\r\n\r\n";
     path p = ".";
     recursive_directory_iterator enit(p);
     for(path en : enit)
@@ -19,7 +24,7 @@ void dirent()
         if(en.has_extension() && validate_ext(en.extension()))
         {
             if((status(en).permissions() & perms::owner_exec) != perms::owner_exec)
-                std::cout << en.string().substr(2) << '\n';
+                std::cout << mkelem(en.string().substr(2)) << '\n';
         }
     }
 }
